b1f070f199188fddf91e3537169c5b2a678e9c1c
JBasilio24 <30379691+JBasilio24@users.noreply.github.com>
2018-07-07 10:59:06 -0500

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,51 +1,42 @@
-from decimal import Decimal
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.utils.translation import ugettext as _
+import requests
+import time
+import serial
+
+def get_dif(first, now):
+    dif = now-first
+    percentage = (dif/now)*100
+    return percentage
+
+def get_price(coin_name):
+    resp = requests.get(url="https://api.bitso.com/v3/ticker/?book="+coin_name)
+    if resp.status_code == 200:
+        coin = resp.json()
+        if coin['success'] == True:
+            price = coin['payload']['last']
+            return price
 
 
-class User(AbstractUser):
+try:
+    btc_dif = 0
+    eth_dif = 0
+    trigger = 0
+    while True:
+        print(trigger)
+        if trigger == 0:
+            btc_mxn_first = get_price("btc_mxn")
+            eth_mxn_first = get_price("eth_mxn")
+        trigger = trigger+1
+        btc_mxn_now = get_price("btc_mxn")
+        eth_mxn_now = get_price("eth_mxn")
+        if trigger == 2:
+            btc_dif = get_dif(float(btc_mxn_first), float(btc_mxn_now))
+            eth_dif = get_dif(float(eth_mxn_first), float(eth_mxn_now))
+            trigger = 0
+        print(btc_mxn_now)
+        print(eth_mxn_now)
+        print(eth_dif)
+        print(btc_dif)
+        time.sleep(30)
 
-    coins = models.DecimalField(max_digits=20, decimal_places=4, default=Decimal('0.0'))
-
-    class Meta(AbstractUser.Meta):
-        swappable = 'AUTH_USER_MODEL'
-
-        permissions = (
-            ('can_sell', _('Puede Vender')),
-            ('can_see_sales', _('Puede Ver Ventas')),
-            ('can_see_commands', _('Puede Ver Comandas')),
-            ('can_assemble', _('Puede Ensamblar')),
-            ('can_see_suplies', _('Puede Ver Insumos')),
-            ('can_see_cartrdiges', _('Puede Ver Cartuchos')),
-            ('can_see_packages', _('Puede Ver Paquetes')),
-            ('can_see_storage_analytics', _('Puede Ver Estadísticas de Almacén')),
-            ('can_see_suggestions', _('Puede Ver Comentarios')),
-            ('can_see_suggestions_analytics', _('Puede Ver Estadísticas de Comentarios')),
-        )
-
-
-class UserMovements(models.Model):
-    user = models.CharField(max_length=20, default='None')
-    category = models.CharField(max_length=20,default='None')
-    creation_date = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return self.user
-
-class Job(models.Model):
-    ELEMENTARY = 'PR'
-    MIDDLE = 'SC'
-    HIGHT = 'PP'
-    UNI = 'LC'
-
-    GRADE = (
-        (ELEMENTARY, 'Primaria'),
-        (MIDDLE, 'Secundari'),
-        (HIGHT, 'Prepearatoria'),
-        (UNI, 'Licenciatura'),
-    )
-
-    name = models.CharField(max_length=60)
-    age = models.PositiveSmallIntegerField()
-    grade = models.CharField(max_length=2, choices=GRADE)
+except KeyboardInterrupt:
+    print("Succes: False")
